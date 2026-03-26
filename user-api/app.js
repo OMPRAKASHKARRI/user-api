290e04d762b4003125084ae4f69d3a763f611e24
PRAKASHOMVAR <omvaradhan@gmail.com>
2026-03-26 15:20:40 +0530

    final commit

--- user-api/app.js
+++ user-api/app.js
@@ -7,8 +7,7 @@ app.use(express.json());
 // routes
 app.use("/users", userRoutes);
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
